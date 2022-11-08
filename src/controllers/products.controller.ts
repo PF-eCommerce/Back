721860c5a4a21e85765cfcbed00f5e47acd4cbbc
Tomas Oyarzun <xilent312@gmail.com>
2022-11-08 20:35:21 -0300

    git

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -3,7 +3,7 @@ import Category from '../model/Category'
 import {postIProduct, noIdCategory} from '../types'
 import {Request, Response} from 'express'
 
-export const postProduct = async (req : Request , res: Response) : Promise< Response<any, Record<string, any>> | void>  =>  {
+export const postProduct = async (req : Request , res: Response) =>  {
       
     const {title , desc , img, price, numStock } : postIProduct  = req.body
     const {type, size , color} : noIdCategory = req.body
@@ -11,7 +11,8 @@ export const postProduct = async (req : Request , res: Response) : Promise< Resp
     try {
         const existsProduct = await Product.findOne({title})
         if(existsProduct){
-            return res.status(400).json({error : true, msg: 'el producto ya existeeeeeee'})
+             res.status(400).json({error : true, msg: 'el producto ya existeeeeeee'})
+             return
         }
 
         const product =  new Product({title , desc , img, price, numStock})
@@ -19,19 +20,24 @@ export const postProduct = async (req : Request , res: Response) : Promise< Resp
         await new Category({_id : product._id ,type, size , color}).save()
         await product.save();
 
-        return res.status(201).json(product)
+        res.status(201).json(product)
     } catch (error) {
         console.log(error)
     }
 }
 
 export const getProduct = async (_req : Request , res: Response) : Promise< Response<any, Record<string, any>> | void> => {
+     
+ 
+     
     try {
+
         const allProducts = await Product.find()
+      
 
         if(allProducts.length === 0) return res.status(204).json({msg : "no existe ningun product"})
 
-        return res.status(200).json(allProducts)
+         res.status(200).json(allProducts)
     } catch (error) {
         console.log(error)
     }
