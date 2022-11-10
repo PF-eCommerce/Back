f29d6d7f4dd8937e6f13bd3cdb10a0594298b30d
Tomas Oyarzun <xilent312@gmail.com>
2022-11-10 13:20:54 -0300

    cors app

--- src/routes/login/login.routes.ts
+++ src/routes/login/login.routes.ts
@@ -1,17 +1,18 @@
 import express from 'express'
+
 import * as userFunction from '../../controllers/login.controller'
-import {body} from 'express-validator'
+import {verifyToken } from '../../middleware/verifyToken'
+ import {body} from 'express-validator'
 const router = express.Router()
 
 
-router.post('/register/user',  body('password').isLength({ min: 5 }), body('email').isEmail().normalizeEmail(),
- userFunction.postUser)
-
+router.post('/register/user',  body('password').isLength({ min: 5 }), body('email').isEmail().normalizeEmail(), userFunction.postUser)
  router.get('/confirm/:token', userFunction.confirmUser)
  router.post('/login', userFunction.authenticate)
  router.post('/change-password/profile', userFunction.changePassword)
  router.post('forgot-password', userFunction.forgotPassword)
  router.get('/check-token', userFunction.checkToken)
  router.put('/forgot-password/update', userFunction.newPassword)
+ router.get('/perfil' , verifyToken , userFunction.perfil)
 
 export default router
